a13e70afaae660012ed2daf5660dc880597b6d81
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2026-02-04 11:11:31 -0300

    feat: Add Melhor Envio token and CEP_ORIGEM environment variables to Lambda configuration
    Includes MELHOR_ENVIO_TOKEN and EP_ORIGEM variables in the Lambda module configuration to support the Melhor Envio microservice functionality. This addition ensures proper integration with the necessary external services.

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -157,6 +157,8 @@ module "products_lambda" {
     FIREBASE_CLIENT_EMAIL   = var.firebase_client_email
     FIREBASE_PRIVATE_KEY    = var.firebase_private_key
     FIREBASE_DATABASE_URL   = var.firebase_database_url
+    MELHOR_ENVIO_TOKEN      = var.melhor_envio_token
+    EP_ORIGEM               = var.cep_origem
     POWERTOOLS_SERVICE_NAME = "products"
   }
 
