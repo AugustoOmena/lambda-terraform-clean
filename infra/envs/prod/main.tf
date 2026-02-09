f1fb3efe6d894327afdfcf03d2dca84bec44ae76
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2026-02-09 13:12:22 -0300

    fix: Update cleanup schedule for orphan images to 16:13 UTC
    Modifies the CloudWatch event rule for the cleanup of orphan images to run daily at 16:13 UTC, adjusting the schedule expression accordingly. This change ensures timely execution of the cleanup process in both development and production environments.
    
    Files modified:
    - infra/envs/dev/main.tf: Updates schedule for cleanup Lambda function.
    - infra/envs/prod/main.tf: Mirrors schedule change for production environment.

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -379,8 +379,8 @@ module "cleanup_orphan_images_lambda" {
 
 resource "aws_cloudwatch_event_rule" "cleanup_orphan_images_schedule" {
   name                = "loja-omena-cleanup-orphan-images"
-  description         = "Executa limpeza de imagens órfãs diariamente às 11:25 BRT (14:25 UTC)"
-  schedule_expression = "cron(25 14 * * ? *)"
+  description         = "Executa limpeza de imagens órfãs diariamente às 13:13 BRT (16:13 UTC)"
+  schedule_expression = "cron(13 16 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "cleanup_orphan_images" {
